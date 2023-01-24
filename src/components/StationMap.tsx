7db261ab2f81926df36bb4a630807e88956f010e
Toni Virtanen <tonikoodaa@gmail.com>
2023-01-24 16:18:23 +0200

    Altered map component to work fine with StationItem component

--- src/components/StationMap.tsx
+++ src/components/StationMap.tsx
@@ -1,75 +1,47 @@
 /*
-  Show station on map. 
+  Show station on map.
 */
 
-import { useCallback, useEffect, useMemo, useState } from 'react'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import { Button } from 'react-bootstrap';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import { IStationMap } from '../types'
+import { useEffect } from 'react'
 
 const center:any = [60.192059, 24.945831]
-const zoom = 15
+const defaultZoom = 15
 
-// Create marker to current selected station coordinates
-function LocationMarker( {position}:any ):any {
-  return position === null ? null : (
-    <Marker position={[position.y, position.x]}>
-      <Popup>You are here</Popup>
-    </Marker>
-  )
-}
+type ICoords = {
+    lat: number;
+    lng: number;
+};
 
-// Update onview coordinates and show station location when clicked
-function DisplayPosition({ map, coords }:any) {
-  const [position, setPosition] = useState(map.getCenter())
 
-    const onClick = useCallback(() => {
-      map.setView([coords.y, coords.x], zoom)
-  }, [map, coords])
 
-  const onMove = useCallback(() => {
-    setPosition(map.getCenter())
-  }, [map])
-
+// Update onview coordinates and show station location when station changed.
+function ChangeMapView({ position }: { position: ICoords }) {
+  const map = useMap();
   useEffect(() => {
-    map.on('move', onMove)
-    return () => {
-      map.off('move', onMove)
-    }
-  }, [map, onMove])
+    map.setView([position.lat, position.lng], defaultZoom);
+  }, [position, map]);
 
-  return (
-    <p>
-      latitude: {position.lat.toFixed(4)}, longitude: {position.lng.toFixed(4)}{' '}
-      <Button variant="primary" onClick={onClick}>Find Station</Button>
-    </p>
-  )
+    return (
+      <>
+        <Marker position={[position.lat, position.lng]}>
+          <Popup>Station is here</Popup>
+        </Marker>
+      </>
+    );
 }
 
-const StationMap:React.FC<IStationMap> = ( {coords} ) => {
-  const [map, setMap]:any = useState(null)
-
-  const displayMap = useMemo(
-    () => (
-      <MapContainer center={center} zoom={zoom} scrollWheelZoom={true} ref={setMap}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {coords ? <LocationMarker
-          position={coords}
-        />: <></>}
-      </MapContainer>
-    ),
-    [coords],
-  )
-
-  return (
-    <div>
-      {map ? <DisplayPosition map={map} coords={coords} /> : null}
-      {displayMap}
-    </div>
-  )
+const StationMap: React.FC<IStationMap> = ({ stationPosition }) => {
+      return(
+        <MapContainer center={center} zoom={defaultZoom} scrollWheelZoom={true}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <ChangeMapView position={stationPosition} />
+        </MapContainer>
+      )
 }
 
 export default StationMap
